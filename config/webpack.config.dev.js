72af19222a8600982465ddb2f184f63feec065fe
wutao <m15776849661@163.com>
2020-07-23 13:28:54 +0800

    feat: bingo

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,17 @@
+import path from 'path'
+module.exports = {
+  entry: '../src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  },
+  plugins: []
+}
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const View = () => {
+  return (
+    <div>单页入口</div>
+  )
+}
+
+ReactDOM.render(<View />, document.getElementById('root'))
