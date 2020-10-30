df2a93a8e17cc0d552191ccf831ddb4a35186659
wutao <m15776849661@163.com>
2020-10-30 16:57:58 +0800

    feat: add loader

--- spa/config/webpack.config.default.js
+++ spa/config/webpack.config.default.js
@@ -1,18 +1,25 @@
 import path from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 
+// const appDirectory = fs.realpathSync(process.cwd())
+
 module.exports = {
   mode: 'development',
   entry: '../src/index.tsx',
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: 'bundle.js'
+    filename: '[name].[id].bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader'
+        loader: 'awesome-typescript-loader',
+        // include: ''
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader'
       }
     ]
   },
